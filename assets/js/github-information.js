07922bf5362909ec64db702c6a815e7ebf258ba9
Antonio-dev7 <antonio.emk28@gmail.com>
2020-07-05 17:41:19 +0000

    Fix bugs the gh-user-data and gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,4 @@
-function userInformationHTML(user) {     //new function above.calling when promise is resolved.return`` template literal using the back quote notation.
+function userInformationHTML(user) {        //new function above.calling when promise is resolved.return`` template literal using the back quote notation.
     return `                                    
         <h2>${user.name}
             <span class="small-name">
@@ -8,16 +8,82 @@ function userInformationHTML(user) {     //new function above.calling when promi
         <div class="gh-content"> 
             <div class="gh-avatar">
                 <a href="${user.html_url}" target="_blank"> 
-                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login /*so it appears in a nice square.*/}" /> 
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login/*so it appears in a nice square.*/}" /> 
                 </a>
             </div>
             <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
+function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");                                            //new
+    $("#gh-repo-data").html("");                                        //new
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
+    }
+
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),              // new
+        $.getJSON(`https://api.github.com/users/${username}/repos`)         // new
+    ).then(
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];                                // new
+            var repoData = secondResponse[0];                               // new
+            $("#gh-user-data").html(userInformationHTML(userData));         // new
+            $("#gh-repo-data").html(repoInformationHTML(repoData));         // new
+        },
+        function(errorResponse) {
+            if (errorResponse.status === 404) {
+                $("#gh-user-data").html(
+                    `<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
+}
+
+$(document).ready(fetchGitHubInformation);
 
 
 
+
+
+
+
+
+
+/*
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();  
@@ -29,7 +95,7 @@ function fetchGitHubInformation(event) {
 $("#gh-user-data").html(
     `<div id="loader">
         <img src="assets/css/loader.gif" alt="loading..." />
-    </div>`); 
+    </div>`);  
 
 $.when(
     $.getJSON(`https://api.github.com/users/${username}`)
@@ -45,7 +111,6 @@ $.when(
             console.log(errorResponse);
             $("gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);   //} }bottom
         }
-
     });
-
 }
+*/
